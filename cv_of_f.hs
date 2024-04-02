65449d71f0c58d20b3a0007d23c3ea318f4a8761
Alex Hellberg <81573329+alexbergh@users.noreply.github.com>
2024-04-02 19:45:34 +0500

    Update and rename cv.hs to cv_of_f.hs
    The getNextRandom function now uses random to generate a single random number.
    Parallelization with parMap and rseq has been removed, as it may be inefficient for small amounts of data.
    Added a chunk helper function to split the list into parts.

--- cv_of_f.hs
+++ cv_of_f.hs
@@ -1,5 +1,5 @@
 import Data.List (genericLength)
-import System.Random (newStdGen, randomRs)
+import System.Random (newStdGen, randomR)
 import Control.Monad.Random (Rand, RandomGen, evalRand)
 import Control.Monad (replicateM)
 import Control.Parallel.Strategies (parMap, rseq)
@@ -15,33 +15,38 @@ avgBent xs = sum (map bent xs) / genericLength xs
 -- Cross-validation
 crossValidation :: RandomGen g => Int -> g -> [[Double]] -> Double
 crossValidation k g xs =
-  let splits = splitList k xs
-      scores = parMap rseq avgBent splits
-  in sum scores / genericLength scores
+ let splits = splitList k xs
+      scores = map avgBent splits
+ in sum scores / genericLength scores
 
 -- Dividing data into k equal parts
 splitList :: Int -> [a] -> [[a]]
 splitList k xs =
-  let n = length xs div k
-  in take k $ chunk n xs
+ let n = length xs `div` k
+ in take k $ chunk n xs
 
 -- Main function
 main :: IO ()
 main = do
-  let k = 5 -- number of cross-validation folds
-  let n = 100 -- number of data samples
+ let k = 5 -- number of cross-validation folds
+ let n = 100 -- number of data samples
   
-  -- Random Data Generation
-  g <- newStdGen
-  let randomData = evalRand (replicateM n (replicateM 10 (getNextRandom g))) g
+ -- Random Data Generation
+ g <- newStdGen
+ let randomData = evalRand (replicateM n (replicateM 10 (getNextRandom g))) g
   
-  -- cross-validation Calculation
-  let cvScore = crossValidation k g randomData
+ -- cross-validation Calculation
+ let cvScore = crossValidation k g randomData
   
-  putStrLn $ "Average value of benchmark functions: " ++ show cvScore
-  where
+ putStrLn $ "Average value of benchmark functions: " ++ show cvScore
+ where
     -- Generate random numbers from -1 to 1
     getNextRandom :: RandomGen g => g -> Rand g Double
     getNextRandom g = do
-      let (r, g') = randomRs (-1, 1) g
-      return $ head r
+      let (r, g') = randomR (-1, 1) g
+      return r
+
+    -- Helper function to split a list into chunks
+    chunk :: Int -> [a] -> [[a]]
+    chunk _ [] = []
+    chunk n xs = take n xs : chunk n (drop n xs)
